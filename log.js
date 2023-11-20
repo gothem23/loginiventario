99b17999574b7908de6c37e7539d293e7e4f06ad
gothem23 <149277359+gothem23@users.noreply.github.com>
2023-11-20 12:55:31 -0600

    Add files via upload

---
+++ log.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById('loginForm');
+const loginContainer = document.getElementById('loginContainer');
+const mainContent = document.getElementById('mainContent');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe
+  
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  
+  // Aquí deberías realizar la lógica de autenticación, por ejemplo, verificar las credenciales con una base de datos o servicio
+  const isAuthenticated = (username === 'usuario' && password === 'contraseña'); // Ejemplo de autenticación
+  
+  if (isAuthenticated) {
+    loginContainer.style.display = 'none'; // Oculta el formulario de inicio de sesión
+    mainContent.style.display = 'block'; // Muestra el contenido principal
+  } else {
+    alert('Credenciales incorrectas. Inténtalo de nuevo.'); // Mensaje de error (puedes cambiarlo por tu propia lógica)
+  }
+});
